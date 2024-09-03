25ced6a0525780d41909b7cf9f433844fe4c0fb2
Hosung.Lim <hosung.lim@banksalad.com>
2024-09-03 23:58:14 +0900

    - Android : App → Activity → Custom Title → 타이틀 변경하기 - iOS : 입력필드1, 입력필드2 숫자 기입 → [Compute Sum] 버튼 선택

---
+++ src/main/kotlin/Android_Setup.kt
@@ -0,0 +1,105 @@
+import com.sun.jdi.Value
+import io.appium.java_client.android.AndroidDriver
+import io.appium.java_client.remote.options.BaseOptions
+import org.testng.annotations.AfterMethod
+import org.testng.annotations.BeforeMethod
+import org.testng.annotations.Test
+import java.net.MalformedURLException
+import java.net.URL
+import org.openqa.selenium.By
+
+class Android_Setup {
+    private lateinit var driver: AndroidDriver
+
+    // 메뉴명
+    private val AppButton: String
+    private val ActiveButton: String
+    private val CustomTitle: String
+    private val LeftEditText: String
+    private val RightEditText: String
+    private val ChangeLeftButton: String
+    private val ChangeRightButton: String
+    private var LeftTitle: String
+    private val RightTitle: String
+
+    init {
+        AppButton = "//android.widget.TextView[@content-desc=\"App\"]"
+        ActiveButton = "//android.widget.TextView[@content-desc=\"Activity\"]"
+        CustomTitle = "//android.widget.TextView[@content-desc=\"Custom Title\"]"
+        LeftEditText = "io.appium.android.apis:id/left_text_edit"
+        RightEditText = "io.appium.android.apis:id/right_text_edit"
+        ChangeLeftButton = "//android.widget.Button[@content-desc=\"Change Left\"]"
+        ChangeRightButton = "//android.widget.Button[@content-desc=\"Change Right\"]"
+        LeftTitle = "io.appium.android.apis:id/left_text"
+        RightTitle = "io.appium.android.apis:id/right_text"
+    }
+
+    @BeforeMethod
+    fun setUp() {
+        val options = BaseOptions()
+            .amend("platformName", "Android")
+            .amend("appium:platformVersion", "13.0")
+            .amend("appium:deviceName", "Pixel 7 Pro API 33")
+            .amend("appium:automationName", "UiAutomator2")
+            .amend("appium:udid", "emulator-5554")
+            .amend("appium:app", "/Users/hosung.lim/Downloads/ApiDemos-debug.apk")
+            .amend("appium:ensureWebviewsHavePages", true)
+            .amend("appium:nativeWebScreenshot", true)
+            .amend("appium:newCommandTimeout", 3600)
+            .amend("appium:connectHardwareKeyboard", true)
+
+        driver = AndroidDriver(getUrl(), options)
+    }
+
+    private fun getUrl(): URL {
+        return try {
+            URL("http://127.0.0.1:4723")
+        } catch (e: MalformedURLException) {
+            e.printStackTrace()
+            throw RuntimeException("Invalid URL")
+        }
+    }
+
+    fun ClickXpath(xPath: String){
+        driver.findElement(By.xpath(xPath)).click()
+    }
+
+    fun ClearText(id: String){
+        driver.findElement(By.id(id)).clear()
+    }
+
+    fun InputText(id: String, text: String){
+        driver.findElement(By.id(id)).sendKeys(text)
+    }
+
+    private fun getTextFromElementById(id: String): String {
+        val element = driver.findElement(By.id(id))
+        return element.text
+    }
+
+    private fun printTitles() {
+        val leftTitleText = getTextFromElementById(LeftTitle)
+        val rightTitleText = getTextFromElementById(RightTitle)
+        println("$leftTitleText $rightTitleText")
+    }
+
+    @Test
+    fun ChangingTitle() {
+        // 테스트 케이스 작성
+        ClickXpath(AppButton)
+        ClickXpath(ActiveButton)
+        ClickXpath(CustomTitle)
+        ClearText(LeftEditText)
+        ClearText(RightEditText)
+        InputText(LeftEditText,"ChangeTitle")
+        InputText(RightEditText,"Homework")
+        ClickXpath(ChangeLeftButton)
+        ClickXpath(ChangeRightButton)
+        printTitles()
+    }
+
+    @AfterMethod
+    fun tearDown() {
+        driver.quit()
+    }
+}
